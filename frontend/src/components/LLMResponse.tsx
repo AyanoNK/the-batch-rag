ee42037593328b312c0847a2f5e2dfabfa8c52b7
Ayano <s.correa.wilches@gmail.com>
2025-05-08 11:12:11 -0500

    implement markdown renderer

--- frontend/src/components/LLMResponse.tsx
+++ frontend/src/components/LLMResponse.tsx
@@ -1,5 +1,6 @@
 import { JSX } from "react";
 import { ChatMessage } from "../types/ChatMessage";
+import Markdown from "react-markdown";
 
 interface LLMMessageProps {
   message: ChatMessage;
@@ -9,7 +10,9 @@ export default function LLMMessage({ message }: LLMMessageProps): JSX.Element {
   return (
     <div className="flex w-full justify-start">
       <div className="max-w-md rounded-t-lg rounded-br-lg border border-b-cyan-800 p-4">
-        <span>{message.text}</span>
+        <span className="whitespace-pre-line">
+          <Markdown>{message.text}</Markdown>
+        </span>
       </div>
     </div>
   );
