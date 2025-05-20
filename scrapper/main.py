f69362c9f9557bc7c9fb9fe8af447227ce68f632
Ayano <s.correa.wilches@gmail.com>
2025-05-20 15:09:16 -0500

    update scrapper script to upload to pinecone

--- scrapper/main.py
+++ scrapper/main.py
@@ -1,20 +1,27 @@
 import asyncio
 import logging
+import os
 import sys
-from boto3 import Session
+
+from pinecone import Pinecone, ServerlessSpec
 
 from scrappers.weekly_issues import scrape_weekly_issues
 
 LOG_NAME = "weekly_issues.log"
 LOG_FORMAT = "%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s"
 
-session = Session(
-    region_name="us-east-1",
-    profile_name="bedrock",
-)
+# session = Session(
+#     region_name="us-east-1",
+#     profile_name="bedrock",
+# )
 
-s3_client = session.client(
-    "s3",
+# s3_client = session.client(
+#     "s3",
+# )
+
+pinecone_api_key = os.getenv("PINECONE_API_KEY")
+pc = Pinecone(
+    api_key=pinecone_api_key,
 )
 
 
@@ -26,9 +33,22 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
+
+INDEX_NAME = "the-batch-index"  # change if desired
+
+if not pc.has_index(INDEX_NAME):
+    pc.create_index(
+        name=INDEX_NAME,
+        dimension=1024,
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+    )
+
+index = pc.Index(INDEX_NAME)
+
 logger = logging.getLogger("weekly_issues")
 logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
 if __name__ == "__main__":
-    asyncio.run(scrape_weekly_issues(s3_client, logger))
+    asyncio.run(scrape_weekly_issues(index=index, pinecone_client=pc, logger=logger))
--- scrapper/scrappers/weekly_issues.py
+++ scrapper/scrappers/weekly_issues.py
@@ -1,13 +1,28 @@
 import logging
 import os
-from crawl4ai import AsyncWebCrawler, CrawlerRunConfig
+from uuid import uuid4
 
+from crawl4ai import AsyncWebCrawler, CrawlerRunConfig
+from langchain_community.document_loaders.markdown import UnstructuredMarkdownLoader
+from langchain_nvidia_ai_endpoints import NVIDIAEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from openai import OpenAI
+from pinecone import Pinecone
 
 URL = "https://www.deeplearning.ai/the-batch/issue-{issue_number}/"
-ISSUE_NUMBER = 298
+
+ISSUE_START = 290
+ISSUE_LIMIT = 298
+
+openai_client = OpenAI(
+    base_url="https://integrate.api.nvidia.com/v1",
+    api_key=os.environ.get("NVIDIA_KEY", None),
+)
 
 
-async def scrape_weekly_issues(s3_client, logger: logging.Logger) -> None:
+async def scrape_weekly_issues(
+    index, pinecone_client: Pinecone, logger: logging.Logger
+) -> None:
     """Function to scrape the weekly issues of the deeplearning.ai website.
 
     Args:
@@ -23,11 +38,19 @@ async def scrape_weekly_issues(s3_client, logger: logging.Logger) -> None:
         log_console=True,
     )
 
+    if not os.getenv("PINECONE_API_KEY"):
+        raise ValueError("PINECONE_API_KEY environment variable not set.")
+
     async with AsyncWebCrawler(
         exclude_external_links=True,
         # exclude_social_media_links=True,
     ) as crawler:
-        for issue in range(1, ISSUE_NUMBER + 1):
+        for issue in range(ISSUE_START, ISSUE_LIMIT + 1):
+            # check if the issue already exists
+            if os.path.exists(f"scrapped_issues/issue_{issue}.md"):
+                logger.info("Issue %d already exists, skipping.", issue)
+                continue
+
             # Use lazy % formatting in logging functions
             logger.info("Scraping issue %d", issue)
             result = await crawler.arun(
@@ -39,17 +62,50 @@ async def scrape_weekly_issues(s3_client, logger: logging.Logger) -> None:
                 logger.error("Failed to scrape issue %d", issue)
                 raise Exception(f"Failed to scrape issue {issue}: {result.error}")
 
-            with open(f"issue_{issue}.md", "w", encoding="utf-8") as f:
+            with open(f"scrapped_issues/issue_{issue}.md", "w", encoding="utf-8") as f:
                 f.write(result.markdown)
 
-            # save file in S3
-            logger.info("Uploading issue %d to S3", issue)
-            s3_client.upload_file(
-                Filename=f"issue_{issue}.md",
-                Bucket="the-batch-markdown",
-                Key=f"issue_{issue}.md",
-            )
+            # save_to_s3(
+            #     s3_client,
+            #     file_path=f"scrapped_issues/issue_{issue}.md",
+            #     bucket_name="the-batch-markdown",
+            #     object_name=f"scrapped_issues/issue_{issue}.md",
+            # )
+    save_to_pinecone(index=index, pinecone_client=pinecone_client)
 
-            # remove file
-            logger.info("Removing issue %d file", issue)
-            os.remove(f"issue_{issue}.md")
+
+def save_to_s3(s3_client, file_path, bucket_name, object_name):
+    """Upload a file to an S3 bucket."""
+    try:
+        s3_client.upload_file(file_path, bucket_name, object_name)
+        print(f"Successfully uploaded {file_path} to {bucket_name}/{object_name}")
+    except Exception as e:
+        print(f"Error uploading file to S3: {e}")
+
+
+def save_to_pinecone(index, pinecone_client: Pinecone):
+    embeddings = NVIDIAEmbeddings(
+        nvidia_api_key=os.environ.get("NVIDIA_KEY", ""), model="baai/bge-m3"
+    )
+
+    vector_store = PineconeVectorStore(index=index, embedding=embeddings)
+
+    documents = []
+
+    # open one by one files in a directory
+    for issue in range(ISSUE_START, ISSUE_LIMIT + 1):
+        markdown_path = f"scrapped_issues/issue_{issue}.md"
+        loader = UnstructuredMarkdownLoader(
+            file_path=markdown_path,
+            mode="elements",
+            strategy="hi_res",
+        )
+        loaded_document = loader.load()
+
+        for document in loaded_document:
+            if len(document.page_content) > 100:
+                # add metadata to the document
+                documents.append(document)
+
+    uuids = [str(uuid4()) for _ in range(len(documents))]
+    vector_store.add_documents(documents=documents, ids=uuids)
